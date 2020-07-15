e897c14b6550891a982ae649206748521d95b452
tttyyy1456 <tttyyy1456@gmail.com>
2020-07-15 23:44:27 +0400

    fix spelling

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image.js');
 const db = knex({
 	client: 'pg',
 	connection: {
-		connectionString: process.DATABASE_URL,
+		connectionString: process.env.DATABASE_URL,
 		ssl: true
 	}
 });
